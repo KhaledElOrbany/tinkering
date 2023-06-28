b1eca39f00e64dba9edab14264bc4fa61959ed69
elorbany <khaled.wagdy554@gmail.com>
2023-06-29 00:34:42 +0300

    added redux-toolkit

---
+++ react/redux-toolkit/features/cake/cakeSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+
+const initialState = {
+  numOfCakes: 10,
+}
+
+const cakeSlice = createSlice({
+  name: 'cake',
+  initialState,
+  reducers: {
+    order: (state, payload) => {
+      state.numOfCakes -= payload.quantity;
+    },
+    restock: (state, payload) => {
+      state.numOfCakes += payload.quantity;
+    }
+  }
+})
+
+module.exports = cakeSlice.reducer;
+module.exports.cakeActions = cakeSlice.actions;
